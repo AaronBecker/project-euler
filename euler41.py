6bbaec43bf9b970e2622536685106e9629a9afd0
Aaron Becker <aaron@indiscretesoftware.com>
2009-02-24 13:53:56 -0600

    solved 41, with much more efficient approach

--- euler41.py
+++ euler41.py
@@ -1,10 +1,18 @@
-from euler_util import sieve_of_eratosthenes, is_pandigital
+from euler_util import permutations
+
+def is_prime(x):
+    for i in xrange(2, int(x**0.5) + 1):
+        if x % i == 0: return False
+    return True
 
 def euler41():
     """What is the largest n-digit pandigital prime?"""
-    for prime in sieve_of_eratosthenes(10 ** 9).reverse():
-        if is_pandigital(prime):
-            print 'The largest pandigital prime is %d' % prime
-            return prime
-    return 0
+    primes = []
+    # note: sum(1..8) % 3 == sum(1..9) % 3 == 0, so 7 digits is the max
+    for perm in permutations("1234567"):
+        if is_prime(int(perm)):
+            primes.append(int(perm))
+    primes.sort()
+    print 'The largest pandigital prime is %d' % primes[-1]
+    return primes[-1]
 
