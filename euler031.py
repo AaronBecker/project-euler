e58196d4fa6a7506f9b7a7032d2ad808844f2374
Aaron Becker <akbecker@gmail.com>
2011-03-25 17:40:58 -0500

    solved 31

--- euler031.py
+++ euler031.py
@@ -1,15 +1,17 @@
 
-coins = [1, 2, 5, 10, 20, 50, 100, 200]
+uk_coins = [1, 2, 5, 10, 20, 50, 100, 200]
+
+def change_count(target, total, coin, coins):
+    if total == target:
+        return 1
+    if total < target and len(coins) > 0 and coin <= coins[0]:
+        return change_count(target, total + coins[0], coins[0], coins) + \
+                change_count(target, total, coin, coins[1:])
+    return 0
 
 def euler31(target=200):
-    #FIXME: needs to be order-invariant
     """How many different ways can 2 GBP be made using any number of coins?"""
-    memo = {0:1}
-    for i in range(1, target+1):
-        ways = 0
-        for c in coins:
-            if memo.has_key(i-c):
-                ways = ways + memo[i-c]
-        memo[i] = ways
-    print "There are %d ways to make %d p from change" % (target, memo[target])
-    return memo[target]
+    ways = change_count(200, 0, 1, uk_coins)
+    print "There are %d ways to make %d p from change" % (ways, target)
+    return ways
+
