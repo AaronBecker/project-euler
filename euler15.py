2b4e65c3d6273dc1582c8825dd622d0494550e59
Aaron Becker <akbecker@gmail.com>
2011-03-24 22:09:29 -0500

    get rid of files with old naming convention

--- euler15.py
+++
@@ -1,7 +0,0 @@
-
-def euler15(x=2**1000):
-    """What is the sum of the digits of the number 2**1000?"""
-    digit_sum = sum([int(d) for d in str(x)])
-    print "The sum of the digits of is %d" % digit_sum
-    return digit_sum
-
--- euler20.py
+++
@@ -1,8 +0,0 @@
-from euler_util import factorial
-
-def euler20(x=100):
-    """Find the sum of digits in 100!"""    
-    s = sum([int(c) for c in str(factorial(x))])
-    print 'The sum of the digits of %d is %d' % (x, s)
-    return s
-
