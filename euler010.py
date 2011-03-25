a2af486fd050301fe96c532a8f14a65b466fd568
Aaron Becker <akbecker@gmail.com>
2011-03-24 21:54:14 -0500

    added docstring

--- euler010.py
+++ euler010.py
@@ -1,6 +1,7 @@
 from euler_util import sieve_of_eratosthenes
 
 def euler10(target=1000000):
+    """Calculate the sum of all the primes below two million."""
     prime_sum = sum(sieve_of_eratosthenes(target))
     print "The sum of all primes less than %d is %d." % (target, prime_sum)
     return prime_sum
