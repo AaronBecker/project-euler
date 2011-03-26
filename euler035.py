230c7d7f2a63fe251668ba4abaad3d0d590178d4
Aaron Becker <akbecker@gmail.com>
2011-03-26 17:47:30 -0500

    solved 35

---
+++ euler035.py
@@ -0,0 +1,27 @@
+
+from euler_util import sieve
+from math import log10
+import re
+
+def cycles(x):
+    yield x
+    e = 10 ** int(log10(x))
+    for i in range(int(log10(x))):
+        ones = x % 10
+        x = x/10 + ones*e
+        yield x
+
+primes = []
+template = re.compile("[1379]+")
+def cyclic_prime(x):
+    if x > 9 and not template.match(str(x)):
+        return False
+    return all(y in primes for y in cycles(x))
+
+def euler35(upper_bound=1000000):
+    global primes
+    primes = sieve(upper_bound)
+    prime_count = sum(cyclic_prime(x) for x in primes)
+    print 'There are %d circular primes under %d' % (prime_count, upper_bound)
+    return prime_count
+
