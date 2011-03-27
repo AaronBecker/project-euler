d1b2398aed32fbd2733d51d2e3573a2c01573f83
Aaron Becker <akbecker@gmail.com>
2011-03-27 14:10:36 -0500

    solved 33

---
+++ euler033.py
@@ -0,0 +1,24 @@
+
+from fractions import Fraction
+
+def can_cancel(num, denom):
+    if num >= denom or num % 10 == 0 and denom % 10 == 0: return False
+    sn, sd = str(num), str(denom)
+    if sn[0] in sd:
+        sn, sd = sn[1], sd.replace(sn[0], '', 1)
+    elif sn[1] in sd:
+        sn, sd = sn[0], sd.replace(sn[1], '', 1)
+    else:
+        return False
+    if int(sd) == 0: return False
+    return Fraction(num, denom) == Fraction(int(sn), int(sd))
+
+
+def euler33():
+    """Find the fractions with an unorthodox cancelling method."""
+    fracs = [Fraction(num, denom) for num in range(10, 100) \
+            for denom in range(10, 100) if can_cancel(num, denom)]
+    prod = reduce(lambda x, y: x*y, fracs)
+    print 'The product of the special fractions is %s' % prod
+    return prod.denominator
+
