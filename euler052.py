368ebd66cfd00ceb9e67794d7072a2c7979ff0e9
Aaron Becker <akbecker@gmail.com>
2011-03-27 16:10:23 -0500

    solved 52

---
+++ euler052.py
@@ -0,0 +1,15 @@
+
+from itertools import count, dropwhile
+
+def same_digits(x):
+    return set(str(x)) == set(str(6*x)) == set(str(5*x)) == \
+            set(str(4*x)) == set(str(3*x)) == set(str(2*x))
+
+def euler52():
+    """Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+    contain the same digits in some order."""
+    n = dropwhile(lambda x: not same_digits(x), count(1)).next()
+    print 'The smallest integer x, such that 2x, 3x, ..., 6x all have',
+    print 'the same digits is %d' % n
+    return n
+
