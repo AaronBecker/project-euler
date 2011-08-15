7bfb901a669d8e56dcab5c318e794d527d222249
Aaron Becker <akbecker@gmail.com>
2011-08-15 17:04:34 -0500

    solved 205

--- euler205.py
+++ euler205.py
@@ -12,18 +12,17 @@ def euler205():
     What is the probability that Pyramidal Pete beats Cubic Colin? Give your
     answer rounded to seven decimal places in the form 0.abcdefg.
     """
-
-    peter, colin = [0] * 37, [0] * 37
+    peter, colin, peter_wins = [0] * 37, [0] * 37, 0
     for d1 in range(1, 5):
         for d2 in range(1, 5):
             for d3 in range(1, 5):
-                for d5 in range(1, 5):
+                for d4 in range(1, 5):
                     for d5 in range(1, 5):
                         for d6 in range(1, 5):
                             for d7 in range(1, 5):
                                 for d8 in range(1, 5):
                                     for d9 in range(1, 5):
-                                        peter[d1+d2+d3+d5+d5+d6+d7+d8+d9] += 1
+                                        peter[d1+d2+d3+d4+d5+d6+d7+d8+d9] += 1
     for d1 in range(1, 7):
         for d2 in range(1, 7):
             for d3 in range(1, 7):
@@ -31,9 +30,6 @@ def euler205():
                     for d5 in range(1, 7):
                         for d6 in range(1, 7):
                             colin[d1+d2+d3+d4+d5+d6] += 1
-    odds = 0
-    for c in range(6, 37):
-        odds += float(colin[c]) / sum(colin) * float(sum(peter[c+1:])) / sum(peter)
-    print colin
-    print peter
-    return '%0.7f' % odds
+    for c in range(1, 37):
+        peter_wins += colin[c] * sum(peter[c+1:])
+    return '%0.7f' % (float(peter_wins) / (sum(peter) * sum(colin)))
