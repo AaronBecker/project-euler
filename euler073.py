ea5dad4949b5521ba688316acd82bc3e381897f1
Aaron Becker <akbecker@gmail.com>
2012-07-12 21:09:17 -0500

    fixed error in problem description #73

--- euler073.py
+++ euler073.py
@@ -7,7 +7,7 @@ def euler73(limit=12000):
     Consider the fraction, n/d, where n and d are positive integers. If nd and
     HCF(n,d)=1, it is called a reduced proper fraction.
 
-    If we list the set of reduced proper fractions for d  8 in ascending order
+    If we list the set of reduced proper fractions for d<=8 in ascending order
     of size, we get:
 
     1/8, 1/7, 1/6, 1/5, 1/4, 2/7, 1/3, 3/8, 2/5, 3/7, 1/2, 4/7, 3/5, 5/8, 2/3,
