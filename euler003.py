d3f4e64ed76c3edbbd9721b4ee6964b92f7eff74
Aaron Becker <akbecker@gmail.com>
2012-07-24 05:55:03 -0500

    simplified solution to #3

--- euler003.py
+++ euler003.py
@@ -5,10 +5,8 @@ def euler3(target=317584931803):
     The prime factors of 13195 are 5, 7, 13 and 29.
     What is the largest prime factor of the number 600851475143?
     """
-    i, sqrt_target = 1, int(target ** 0.5) + 1
-    while i < sqrt_target:
-        if target % i == 0:
+    for i in xrange(2, int(target ** 0.5)):
+        while target % i == 0:
             factor, target = i, target / i
-        i += 1
     return factor
 
