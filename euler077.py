9f2a94401638e84c9be66fc7e5cb736685abb945
Aaron Becker <akbecker@gmail.com>
2013-08-19 23:31:52 -0700

    solved 77

--- euler077.py
+++ euler077.py
@@ -1,18 +1,18 @@
 
+import itertools
 from euler_util import sieve, memoized
 
 primes = sieve(10000)
+
 @memoized
 def ways_limit(n, limit):
-    if n < 2: return 0
-    if n == 2 or n == 3: return 1
-    for p in primes:
-        if p<=limit:
-            print "ways_limit(" + str(n-p) + "," + str(p) + ") = " \
-                    + str(ways_limit(n-p, p))
+    if n < 0: return 0
+    if n == 0: return 1
     return sum(ways_limit(n-p, p) for p in primes if p<=limit)
 
+
 def ways(n): return ways_limit(n, n)
+
 
 def euler77(target=5000):
     """http://projecteuler.net/problem=77
@@ -29,12 +29,5 @@ def euler77(target=5000):
     What is the first value which can be written as the sum of primes in over
     five thousand different ways?
     """
-    print ways(8)
-    #print ways(7)
-    #print ways(6)
-    #print ways(5)
-    #print ways(4)
-    #print ways(3)
-    #print ways(2)
-    #return ways(10)
-
+    for i in itertools.count(1):
+        if ways(i) > 5000: return i
