c4581da6256503f642a9b099c804b2f661073de8
Aaron Becker <akb@google.com>
2013-09-27 16:07:04 -0700

    reformat url

--- euler042.py
+++ euler042.py
@@ -4,7 +4,7 @@ def word_score(s):
     return sum(ord(c) - ord('A') + 1 for c in list(s))
 
 def euler42(input=pe42):
-    """http://projecteuler.net/index.php?section=problems&id=42
+    """http://projecteuler.net/problem=42
 
     How many triangle words does the list of common English words contain?"""
     triangle_list = [int(0.5 * x * (x + 1)) for x in range(1, 51)]
