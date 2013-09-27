dc7151eecaf811260fb19a82e506286ac7bd9367
Aaron Becker <akb@google.com>
2013-09-27 16:06:24 -0700

    minor cleanup

--- euler042.py
+++ euler042.py
@@ -1,12 +1,11 @@
 pe42 = eval('[' + open('euler042_input.txt').readlines()[0] + ']')
 
 def word_score(s):
-    return sum([ord(c) - ord('A') + 1 for c in list(s)])
+    return sum(ord(c) - ord('A') + 1 for c in list(s))
 
 def euler42(input=pe42):
     """http://projecteuler.net/index.php?section=problems&id=42
 
     How many triangle words does the list of common English words contain?"""
     triangle_list = [int(0.5 * x * (x + 1)) for x in range(1, 51)]
-    triangle_set = set(triangle_list)
-    return len([word for word in input if word_score(word) in triangle_list])
+    return sum(1 for word in input if word_score(word) in triangle_list)
